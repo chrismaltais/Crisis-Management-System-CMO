b9ef700e88c8e9f9a14da4a5244f4947f4bc4f93
programmer1997 <ajinkyamarathe1997@gmail.com>
2017-10-12 17:31:43 +0800

    Login system structure

--- cmoProject/src/cmoProject/controllers/SampleController.java
+++ cmoProject/src/cmoProject/controllers/SampleController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class SampleController {
     
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @RequestMapping(value = "/sample", method = RequestMethod.GET)
     public String printHello(ModelMap model){
         
         // Add content to replace ${message} with "Hello from the other side"
